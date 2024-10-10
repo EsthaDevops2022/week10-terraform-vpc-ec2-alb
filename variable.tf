f14037151cf5fdc2c93eb8470bb839c3a7ab7e2e
EsthaDevops <esthajames@yahoo.fr>
2024-10-10 04:20:21 -0400

    Jenkinsfile

--- variable.tf
+++ variable.tf
@@ -1,11 +1,15 @@
 variable "ENVIRONMENT" {
-  
+  description = "The environment dev"
+  default = "dev"
 }
 
 variable "REGION" {
-  
+  description = "The AWS region"
+  default = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
+    description = "The instance type"
+    default = "t2.micro"
   
 }
